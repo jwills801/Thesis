f66d2236932d5c4d544a83a4215828cd03b4f2d1
jwills801 <jwills@zagmail.gonzaga.edu>
2026-02-12 20:29:17 -0600

    Included historesis near sliding surface

--- ManualSimulation/TrajectoryTracking.m
+++ ManualSimulation/TrajectoryTracking.m
@@ -23,7 +23,7 @@ Iinf = 1.734e7;
 x_timeDomain = [1.9754, 1.1345, 7.6921];
 
 % Set Valued Control Inputs
-PressureRails = [0 10]*1e6;
+PressureRails = [0 35]*1e6;
 rodArea = 0.15^2*pi; % m^2: Radius squared times pi
 capArea = 1.5*rodArea; % m^2: Area ratio times rod Area
 r= 1.18; % Moment arm from force to torque
@@ -43,7 +43,7 @@ sys = ss(A,B,C,0);
 % Output
 params = struct;
 params.plotFigures = 1;
-params.finalTime = 100;
+params.finalTime = 50;
 params.sys = sys;
 params.controlInputSet = ptoTorqueOptions(:);
 
@@ -57,8 +57,12 @@ function mechPower = dynamics(params)
 dt = 1e-3;
 t = (0:dt:params.finalTime)';
 
+% Control sampling rate
+controlDt = 2e-1;  % Control sampling period
+controlSampleRate = round(controlDt/dt);  % Update every N simulation steps 
+
 % Excitation
-TexcMag = 1.5e6;
+TexcMag = 2e6;
 [~,rampStartInd] = min(abs(t-params.rampTime));
 ramp = 0.5*(1+cos(pi+pi*t/params.rampTime)).*(t<params.rampTime) + (t>=params.rampTime);
 Texc = TexcMag*sin(t*2*pi/params.period).*ramp;
@@ -74,63 +78,79 @@ thetaOpt = cumtrapz(t,thetaDotOpt);
 thetaDDotOpt = [0;diff(thetaDotOpt)./diff(t)];
 lambda = 1;
 k = 1e-5;
+phi = 1e-2; % band around sliding surface
     
 % Simulate
 states = NaN(length(params.sys.A),length(t)); 
 % u = NaN(1,length(t));
 states(:,1) = zeros(length(params.sys.A),1);
+
 for timeInd = 1:length(t)-1
-    % Control
+
     thetaDot(timeInd) = states(1,timeInd);
     theta(timeInd) = states(2,timeInd);
 
+    % Update control input at slower rate
+    if mod(timeInd, controlSampleRate) == 0 || timeInd == 1
 
-    % PI control
+        % PI control
         % u(timeInd) = -1*(Kp*thetaDot(timeInd) + Ki*theta(timeInd));
-    
-    % Sliding Mode Contrl
-    thetaError = theta(timeInd)-thetaOpt(timeInd);
-    thetaDotError = thetaDot(timeInd)-thetaDotOpt(timeInd);
-        % Estimate ThetaDDot without u
-    statesDotHat = params.sys.A*states(:,timeInd) + params.sys.B*Texc(timeInd);
-    thetaDDotError = statesDotHat(1) - thetaDDotOpt(timeInd);
-    
-    s = thetaDotError+lambda*thetaError;
-        % Set sdot to -k*sign(s)
-    u(timeInd) = (-thetaDDotError - lambda*thetaDotError - k*sign(s))/params.sys.B(1);
-    
-    % Select closest force 
-    % [~,controlInd] = min(abs(params.controlInputSet-u(timeInd)));
-    % u(timeInd) = params.controlInputSet(controlInd);
 
-    
-    if s > 0
-        % Pick the largest control option still smaller than u
-        smallerOptions = params.controlInputSet(find(params.controlInputSet<=u(timeInd)));
-        if ~isempty(smallerOptions)
-            u(timeInd) = max(smallerOptions);
-        else
-            u(timeInd) = min(params.controlInputSet);
-        end
-    else
-        % Pick the smallest option still larger than u
-        largerOptions = params.controlInputSet(find(params.controlInputSet>=u(timeInd)));
-        if ~isempty(largerOptions)
-            u(timeInd) = min(largerOptions);
-        else
-            u(timeInd) = max(params.controlInputSet);
-        end
- 
+        % Sliding Mode Contrl
+        thetaError = theta(timeInd)-thetaOpt(timeInd);
+        thetaDotError = thetaDot(timeInd)-thetaDotOpt(timeInd);
+        % Estimate ThetaDDot without u
+        statesDotHat = params.sys.A*states(:,timeInd) + params.sys.B*Texc(timeInd);
+        thetaDDotError = statesDotHat(1) - thetaDDotOpt(timeInd);
+
+        s = thetaDotError+lambda*thetaError;
+        S(timeInd) = s;
+        % if s is large, recalulate control input
+        if abs(s) > phi || timeInd == 1
+
+            % Set sdot to -k*sign(s)
+            u(timeInd) = (-thetaDDotError - lambda*thetaDotError - k*sign(s))/params.sys.B(1);
+
+            % Select closest force
+            % [~,controlInd] = min(abs(params.controlInputSet-u(timeInd)));
+            % u(timeInd) = params.controlInputSet(controlInd);
+
+            % Quantize u based on force options
+            if s > 0
+                % Pick the largest control option still smaller than u
+                smallerOptions = params.controlInputSet(find(params.controlInputSet<=u(timeInd)));
+                if ~isempty(smallerOptions)
+                    u(timeInd) = max(smallerOptions);
+                else
+                    u(timeInd) = min(params.controlInputSet);
+                end
+            else
+                % Pick the smallest option still larger than u
+                largerOptions = params.controlInputSet(find(params.controlInputSet>=u(timeInd)));
+                if ~isempty(largerOptions)
+                    u(timeInd) = min(largerOptions);
+                else
+                    u(timeInd) = max(params.controlInputSet);
+                end
+            end % Quantize u based on force options
+
+        else % If s is small, use previous control input
+            u(timeInd) = u(timeInd-1);
+        end % if statement on size of s
+
+    else % Times between control steps use zero order hold
+        u(timeInd) = u(timeInd-1);
     end
 
 
     % Forward Euler Step
     states(:,timeInd + 1) = states(:,timeInd) + (params.sys.A*states(:,timeInd) + params.sys.B*(u(timeInd)+Texc(timeInd)))*dt;
 end
+
 % For the last time step
 thetaDot(timeInd+1) = states(1,timeInd+1);
 theta(timeInd+1) = states(2,timeInd+1);
-u(timeInd+1) = -1*(Kp*thetaDot(timeInd+1) + Ki*theta(timeInd+1));
+u(timeInd+1) = u(timeInd);
     
 % Absorbed Power
 mechPower = -u.*thetaDot;
@@ -144,11 +164,11 @@ disp([num2str((avePowOpt-avePow)/avePowOpt*100),'% from optimal'])
 
 if params.plotFigures
     figure
-    subplot(221), plot(t,u), xlabel('Time [s]'), ylabel('Control Input [Nm]')
-    subplot(222), yyaxis left, plot(t,thetaDot), ylabel('Angular Velocity [rad/s]')
+    subplot(221), plot(t,u), xlabel('Time [s]'), ylabel('Control Input [Nm]'), grid
+    subplot(222), yyaxis left, plot(t,thetaDot), ylabel('Angular Velocity [rad/s]'), grid
                yyaxis right,plot(t,Texc), xlabel('Time [s]'), ylabel('Excitaiton Torque [Nm]')
-    subplot(223), plot(t,thetaDot,'k',t,thetaDotOpt,'k--'), xlabel('Time [s]'), ylabel('Angular Velocity [W]'), legend('Actual','Optimal')
-    subplot(224), plot(t,mechPower), xlabel('Time [s]'), ylabel('Absorbed Energy [J]')
+    subplot(223), plot(t,thetaDot,'k',t,thetaDotOpt,'k--'), xlabel('Time [s]'), ylabel('Angular Velocity [W]'), legend('Actual','Optimal'), grid
+    subplot(224), plot(t,[0,S]), xlabel('Time [s]'), ylabel('Sliding Surface'), grid
 end
 
 end
