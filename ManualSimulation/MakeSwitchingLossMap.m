b2973583f35c74134ec7055b449e258ee83170d8
jwills801 <jwills@zagmail.gonzaga.edu>
2026-03-12 20:42:01 -0500

    add switching losses

---
+++ ManualSimulation/MakeSwitchingLossMap.m
@@ -0,0 +1,216 @@
+function out = MakeSwitchingLossMap(params)
+
+% This code will make a tabulated grid of points 
+% which can be interpolated between to find the energy loss
+tic
+%% Parameters
+capArea = params.capArea;
+hoseVolume = 0.1^2*pi*5; % m^3: Radius of hose squared times pi times length of the hose
+stroke = params.stroke;
+PR = params.PR;
+
+% Fluid properties
+beta = 1.8e9; % Pa: bulk modulus
+alpha = .03; % Percent entrained air
+
+% Precompute hydraulic effort vectors
+p0 = 101325; % Pa, atmospheric
+HydraulicEffort = hydraulic_effort(beta,alpha);
+
+% Valve constants
+    % set valve constant k: Q = k*sqrt(deltaP)
+valveConstant = 1*capArea/sqrt(5e6); % m^3/s/Pa: Speed of cylinder times area divided by sqrt of allowed pressure drop
+valveDampingRatio = 0.7;
+valveNaturalFrequency = 25*2*pi; % 25 Hz, 25*2*pi rad/s - takes about 20 ms to open
+valveTF = tf(valveNaturalFrequency^2,[1,2*valveNaturalFrequency*valveDampingRatio,valveNaturalFrequency^2]);
+
+% Time parameters
+finalTime = 0.1;
+dt = 1e-5;
+tspan = 0:dt:finalTime;
+
+% Set the delay indices to be used
+    % These are amounts of indecies to be delayed
+    % The amounds of time span from 0 to 0.02 seconds
+maxdelay = round(.02/dt);
+delayvals = linspace(1,maxdelay,11);
+
+% Set the volume and velocity*Area parameters
+velA_n = 11;
+velA_vals = linspace(-1.5*capArea,1.5*capArea,velA_n);
+
+vol_n = 11;
+vol_vals = linspace(hoseVolume,hoseVolume+stroke*capArea,vol_n);
+
+% Initialize matrices
+Eloss = NaN(length(PR),length(PR),velA_n,vol_n); delaychosen = NaN(length(PR),length(PR),velA_n,vol_n);
+Eloss_delay = NaN(size(delayvals));
+
+waitbarhandel = waitbar(0,'Calculating Switching Losses');
+for m = 1:length(vol_vals)
+    waitbar((m-1)/vol_n,waitbarhandel)
+    for i = 1:length(velA_vals)
+        for j = 1:length(PR)
+            for k = 1:length(PR)
+                if j == k % Pressures are the same, no switch occurs
+                    % Loss is just the open valve loss
+                    Eloss(j,k,i,m) = (abs(velA_vals(i)))^3/(valveConstant^2)*finalTime;
+
+                    % Choose the first delay index
+                    delaychosen(j,k,i,m) = 1;
+                else % The pressures are different - a switch occurs
+                    
+                    % Loop over delay values
+                    % Initialize loss vector for each delay
+                    Eloss_delay(:) = NaN;
+                    for ii = 1:length(delayvals)
+                        delay = round(delayvals(ii));
+
+                        % Get valve trajectories
+                        xon = step(valveTF,tspan);
+                        xoff = (1-xon);
+                        xon = [zeros(delay,1);xon(1:end-delay)];
+
+                        % The is no oscillation in the valve. once it opens, it stays open
+                        % Once it closes it closes
+                        xoff((find(xoff<0,1)):end) = 0;
+                        xon((find(xon>1,1)):end) = 1;
+
+                        % Simulate pressure dynamics
+                        % Initialize vectors
+                        PA = NaN(size(tspan')); Q_C = NaN(length(tspan)-1,1); Q_O = NaN(length(tspan)-1,1);
+                        PA(1) = PR(j);
+                        for kk=1:length(tspan)-1
+                            Q_C(kk) = valveConstant*xoff(kk)*sign(PR(j)-PA(kk)).*sqrt(abs(PR(j)-PA(kk))); % Oriface equation for closing valve
+                            Q_O(kk) = valveConstant*xon(kk)*sign(PR(k)-PA(kk)).*sqrt(abs(PR(k)-PA(kk))); % Oriface equation for opening valve
+                            [beta_mix, ~, ~] = oil_comp(PA(kk)+p0, alpha, beta); % Effective bulk modulus
+                            %beta_mix = beta_oil;
+                            PA(kk+1) = PA(kk) + beta_mix/vol_vals(i)*(Q_C(kk)+Q_O(kk)-velA_vals(i))*dt; % Compressibility -> expression for dPdt
+                        end
+                        % calculate losses
+                        % Loss_h = Q_C.*(PR(j)-PA(1:length(tspan)-1));
+                        % Loss_l = Q_O.*(PR(k)-PA(1:length(tspan)-1));
+                        phi_h = interp1(HydraulicEffort.Pvals,HydraulicEffort.phivals,PR(j)+p0);
+                        PA_sat = PA(1:length(tspan)-1); PA_sat(PA_sat<min(HydraulicEffort.Pvals)) = min(HydraulicEffort.Pvals); % Hydraulic effort function only goes so low in pressure
+                        phi_A = interp1(HydraulicEffort.Pvals,HydraulicEffort.phivals,PA_sat(1:length(tspan)-1)+p0);
+                        phi_l = interp1(HydraulicEffort.Pvals,HydraulicEffort.phivals,PR(k)+p0);
+                        Loss_h = Q_C.*(phi_h-phi_A);
+                        Loss_l = Q_O.*(phi_l-phi_A);
+                        Eloss_delay(ii) =  sum(Loss_h+Loss_l)*dt;
+                        % if m == 1 && i == 1 && j == 2 && k == 3
+                        %     figure, title(['Delay value: ',num2str(delay)])
+                        %     subplot(121), plot(tspan,PA)
+                        %     subplot(122), plot(tspan,xon,tspan,xoff)
+                        % end
+                    end
+                    [Eloss(j,k,i,m), delaychosen(j,k,i,m)] = min(Eloss_delay);
+                end
+            end
+        end
+    end
+end
+
+close(waitbarhandel)
+disp(['Making switching losses took ' num2str(toc) ' seconds'])
+
+out = struct();
+out.Eloss = Eloss;
+out.valveConstant = valveConstant;
+out.velA_vals = velA_vals;
+out.vol_vals = vol_vals;
+out.PR = PR;
+end
+
+
+function out = hydraulic_effort(beta_oil,alpha)
+p0 = 101325; % Pa, atmospheric
+pg_vals = logspace(5,8,10);
+phi_vals = NaN(size(pg_vals));
+if alpha == 0
+    phi_vals = [0.0010,0.0022,0.0047, 0.0101,0.0218,0.0469,0.1013,0.2189,0.4749,1.0386]*1e8;
+else
+    waitbarhandel = waitbar(0,'Precomputing Hydraulic Effort');
+    for i = 1:length(pg_vals)
+        waitbar((i-1)/length(pg_vals),waitbarhandel);
+
+        phi_vals(i) = phi(pg_vals(i),p0,alpha,beta_oil);
+        %phi_vals(i)/pg_vals(i)
+    end
+    close(waitbarhandel)
+end
+
+% output results
+out.Pvals = pg_vals;
+out.phivals = phi_vals;
+
+% % Plot results
+% figure, semilogx(pg_vals+p0,phi_vals./pg_vals)
+% xlabel('Pressure [Pa]'), ylabel('\phi(p_g,p_0)/p_g'), grid
+
+% figure, semilogx(pg_vals+p0,phi_vals01./pg_vals,pg_vals+p0,phi_vals03./pg_vals,pg_vals+p0,phi_vals05./pg_vals)
+% xlabel('Pressure [Pa]'), ylabel('\phi(p_g,p_0)/p_g'), grid
+% legend('\alpha = 1%','\alpha = 3%','\alpha = 5%',Location='northwest')
+
+end
+
+
+function phi = phi(pg,p0,alpha,beta_oil)
+dp = pg/1e2;
+P_vals = p0:dp:(pg+p0);
+phi = 0;
+for i = 1:length(P_vals)
+    phi = phi + exp(g(pg+p0,P_vals(i),alpha,beta_oil))*dp;
+end
+end
+
+function g = g(p2,p1,alpha,beta_oil)
+dp = 100;
+P_vals = p1:dp:p2;
+g = 0;
+for i = 1:length(P_vals)
+    g = g + dp/oil_comp(P_vals(i), alpha, beta_oil);
+end
+end
+
+function [beta_mix, W, rv] = oil_comp(P, alpha, beta_oil)
+% Computes the bulk modulus, compressibility energy, and volume ratio of
+% the oil based on the entrained air. Used in the cam synthesis, MATLAB
+% analysis of a single piston, and simulink models. 
+
+    %OIL_COMP compressibility of oil-air mixture
+    %   beta_mix : spot bulk modulus of mixture at P; [Pa]
+    %   W : volumetric energy density [J/m^3] 
+    %   rv : volume ratio
+    %   Pt : Tank pressure
+    %   P : Abs fluid pressure
+    %   alpha : entrained air fraction
+    %   beta_oil : bulk modulus of pure oil
+    Pt = 101325; % Pa, atmospheric
+    gamma = 1.4; % Air constant
+    Pg = P-Pt;   % Gauge pressure
+    r = P/Pt;    % Pressure ratio
+    e = exp(Pg/beta_oil);
+    rv = (1./e+alpha*r.^(-1/gamma))/(1+alpha);      %Volume ratio
+
+    %Note: beta_mix as calculated by Hans Barkei
+    beta_mix = rv*(1+alpha)./(1./(e*beta_oil)+alpha./(gamma*Pt.*r.^(1/gamma+1)));
+    
+    El=beta_oil.*(e-(1+Pg./beta_oil))+Pt.*(e-1);           %Energy in liquid
+    Eg=P./(gamma-1).*(1-r.^(1/gamma-1));          %Energy in gas
+    E=(El./e+alpha*Eg.*r.^(-1/gamma))./(1./e+alpha*r.^(-1/gamma));    %Energy in mixture
+    W = E-(1./rv-1).*Pt;  %Compressibility (volumetric) energy density
+
+    % Code to plot bulk modulus as function of entrained air and pressure
+    % beta_oil = 1.8e9;
+    % alpha = [0 .01 .03 .05];
+    % p0 = 101325;
+    % P = linspace(.1,40e6,50);
+    % 
+    % Beta_mix = NaN(length(alpha),length(P));
+    % for i = 1:length(alpha)
+    %     [Beta_mix(i,:), ~, ~] = oil_comp(p0, P, alpha(i), beta_oil);
+    % end
+    % figure, plot(P/1e6,Beta_mix/1e9), legend('0%','1%','3%','5%'), grid, xlabel('Pressure [MPa]'), ylabel('Beta [GPa]')
+
+end
+
--- ManualSimulation/TrajectoryTracking.m
+++ ManualSimulation/TrajectoryTracking.m
@@ -1,8 +1,100 @@
-clear, close all
+% clear, close all
 tic
-params = getParameters;
-cost = dynamics(params);
+%%
+Params = getParameters;
+
+Control = dynamics(Params);
+%%
+SwitchMap = MakeSwitchingLossMap(Params);
+%%
+Loss = UseSwitchingLossMap(Params,Control,SwitchMap);
+
 toc
+
+function out = UseSwitchingLossMap(Params,Control,SwitchMap)
+    % Calculate volume in each side of the cylinder 
+    d = 7;
+    r_cyl = 3;
+    L_equilib = sqrt(d^2+r_cyl^2 + 2*d*r_cyl*sin(0));
+    L_extra = L_equilib - Params.stroke/2; % All extra length that isnt cap volume length
+    L = sqrt(d^2+r_cyl^2 + 2*d*r_cyl*sin(Control.theta));
+    xCap = L-L_extra;
+    xRod = Params.stroke - xCap;
+    capVol = xCap*Params.capArea;
+    rodVol = xRod*Params.rodArea;
+
+    % Calculate velocity of the cylinder
+    dLdt = d*r_cyl*cos(Control.theta).*Control.thetaDot./L;
+    capVelA = dLdt*Params.capArea;
+    rodVelA = -dLdt*Params.rodArea;
+
+% Find switching events
+    % These denote the start of the switch
+    % The start and end of the simulation are also counted as events
+eventInds = [1, find(diff(Control.u)~=0), length(Control.t)-1];
+eventTimes = Control.t(eventInds);
+
+% Previous and new control values
+switchFrom = Control.u(eventInds);
+switchTo = Control.u(eventInds+1);
+
+mapDT = .1;
+
+% Initilize the energy loss vector
+    % This denotes the energy lost between event times
+    % Thus thereis one less loss entry than there are events
+loss = NaN(length(eventInds)-1,1);
+for k = 1:length(eventInds)-1
+    % penalize switch
+        % Which pressure rail did we switch from
+        % ptoTorqueOptions is a matrix
+            % Each row is a different cap side option
+            % Each col is a different rod side option
+    [capSwitchFromInd,rodSwitchFromInd] = find(switchFrom(k)==Params.controlInputSet);
+    
+        % Which pressure rails are we switching to
+    [capSwitchToInd,rodSwitchToInd] = find(switchTo(k)==Params.controlInputSet);
+
+    capSwitchFrom = Params.PR(capSwitchFromInd);
+    capSwitchTo = Params.PR(capSwitchToInd);
+    capSwitchVelA = capVelA(eventInds(k));
+    capSwitchVol = capVol(eventInds(k));
+    capSwitchingLoss = interpn(SwitchMap.PR,SwitchMap.PR,SwitchMap.velA_vals,SwitchMap.vol_vals, SwitchMap.Eloss,...
+        capSwitchFrom,capSwitchTo,capSwitchVelA,capSwitchVol);
+
+    rodSwitchFrom = Params.PR(rodSwitchFromInd); 
+    rodSwitchTo = Params.PR(rodSwitchToInd);
+    rodSwitchVelA = rodVelA(eventInds(k));
+    rodSwitchVol = rodVol(eventInds(k));
+    rodSwitchingLoss = interpn(SwitchMap.PR,SwitchMap.PR,SwitchMap.velA_vals,SwitchMap.vol_vals, SwitchMap.Eloss,...
+        rodSwitchFrom,rodSwitchTo,rodSwitchVelA,rodSwitchVol);
+
+
+    % Penalize time between switches
+        % What is the flow over this time period 
+    ind1 = eventInds(k) + mapDT/(Control.t(2)-Control.t(1));
+    ind2 = eventInds(k+1);
+    % Find open valve loss from ind1 to ind2
+    capPowerLoss = (abs(capVelA(ind1:ind2))).^3/(SwitchMap.valveConstant^2);
+    rodPowerLoss = (abs(rodVelA(ind1:ind2))).^3/(SwitchMap.valveConstant^2);
+    steadyLoss = trapz(Control.t(ind1:ind2),capPowerLoss+rodPowerLoss);
+
+    loss(k) = capSwitchingLoss + rodSwitchingLoss + steadyLoss;
+end
+TotalValveLoss = sum(loss);
+
+% Plot energy loss per event
+figure, yyaxis right
+plot(eventTimes(1:end-1),loss,'*')
+yyaxis left
+plot(Control.t,Control.u)
+
+% Plot cummulative valve loss over time
+figure, plot(eventTimes(1:end-1),cumsum(loss))
+
+% Output results
+out.TotalValveLoss = TotalValveLoss;
+end
 
 function params = getParameters(~)
 
@@ -24,6 +116,7 @@ x_timeDomain = [1.9754, 1.1345, 7.6921];
 
 % Set Valued Control Inputs
 PressureRails = [0 30]*1e6;
+stroke = 5;
 rodArea = 0.2^2*pi; % m^2: Radius squared times pi
 capArea = 1.5*rodArea; % m^2: Area ratio times rod Area
 r= 1.18; % Moment arm from force to torque
@@ -37,29 +130,36 @@ A = [0,            -Khs/(I+Iinf),0,-1/(I+Iinf);...
     1,               0, 0,        0;...
     0,               0, 0, -x_timeDomain(1);...
 x_timeDomain(3)*1e7, 0, 1, -x_timeDomain(2)];
-B = [1/(I+Iinf);0;0;0]; C = [1,0,0,0];
+B = [1/(I+Iinf);0;0;0]; C = [1,0,0,0]; C = eye(4);
 sys = ss(A,B,C,0);
 
 % Output
 params = struct;
 params.plotFigures = 1;
-params.finalTime = 500;
+params.finalTime = 50;
 params.sys = sys;
-params.controlInputSet = ptoTorqueOptions(:);
+
+params.PR = PressureRails;
+params.controlInputSet = ptoTorqueOptions;
+params.capArea = capArea;
+params.rodArea = rodArea;
+params.stroke = stroke;
 
 % Excitation
 params.period = 10; % s
 params.rampTime = 1e-3; % s
 end
 
-function mechPower = dynamics(params)
+function out = dynamics(params)
 % Time
-dt = 1e-3;
+dt = 1e-2;
 t = (0:dt:params.finalTime)';
 
-% Control sampling rate
-controlDt = 2e-1;  % Control sampling period
-controlSampleRate = round(controlDt/dt);  % Update every N simulation steps 
+% Control Horizon
+controlHorizon = 2e-1;
+
+% How many simulation time steps are there in a control horizon
+HorizonSampling = round(controlHorizon/dt);  % Update every N simulation steps 
 
 % Excitation
 % Regular Waves
@@ -78,7 +178,7 @@ Texc = irregWaves.Texc;
 [~,rampStartInd] = min(abs(t-params.rampTime));
 
 Z = freqresp(params.sys,irregWaves.waveInfo.omega);
-Rr = real(1./squeeze(Z))';
+Rr = real(1./squeeze(Z(1,1,:)))';
 avePowOpt = trapz(irregWaves.waveInfo.omega,abs(irregWaves.F).^2.*irregWaves.waveInfo.spectrum*2/8./Rr)
 
 % PI control
@@ -95,81 +195,82 @@ dw = [0,diff(irregWaves.waveInfo.omega)];
 thetaDotOpt = ramp.* real(sum( exp(1i*(t*irregWaves.waveInfo.omega+phase)) .* ...
     (irregWaves.F/2./Rr.*sqrt(2*irregWaves.waveInfo.spectrum.*dw)) ,2));
 
-figure, plot(t,thetaDotOpt)
-
 % thetaDotOpt = Texc/2/real(1/H(1));
 thetaOpt = cumtrapz(t,thetaDotOpt);
 thetaDDotOpt = [0;diff(thetaDotOpt)./diff(t)];
 lambda = 1;
 k = 1e-5;
-phi = 1e-2; % band around sliding surface
+phi = 3e-2; % band around sliding surface
     
 % Simulate
 states = NaN(length(params.sys.A),length(t)); 
-% u = NaN(1,length(t));
+u = NaN(1,length(t)); u(1) = 0;
 states(:,1) = zeros(length(params.sys.A),1);
 
+waitbarObj = waitbar(0,'Simulating WEC Dynamics');
 for timeInd = 1:length(t)-1
+    waitbar(timeInd/length(t),waitbarObj);
 
     thetaDot(timeInd) = states(1,timeInd);
     theta(timeInd) = states(2,timeInd);
 
-    % Update control input at slower rate
-    if mod(timeInd, controlSampleRate) == 0 || timeInd == 1
+    % Sliding Mode Contrl
+    thetaError = theta(timeInd)-thetaOpt(timeInd);
+    thetaDotError = thetaDot(timeInd)-thetaDotOpt(timeInd);
 
-        % Sliding Mode Contrl
-        thetaError = theta(timeInd)-thetaOpt(timeInd);
-        thetaDotError = thetaDot(timeInd)-thetaDotOpt(timeInd);
-        % Estimate ThetaDDot without u
-        statesDotHat = params.sys.A*states(:,timeInd) + params.sys.B*Texc(timeInd);
-        thetaDDotError = statesDotHat(1) - thetaDDotOpt(timeInd);
+    % Estimate ThetaDDot without u
+    statesDotHat = params.sys.A*states(:,timeInd) + params.sys.B*Texc(timeInd);
+    thetaDDotError = statesDotHat(1) - thetaDDotOpt(timeInd);
 
-        s = thetaDotError+lambda*thetaError;
-        S(timeInd) = s;
-        % if s is large, recalulate control input
-        if abs(s) > phi || timeInd == 1
+    s(timeInd) = thetaDotError+lambda*thetaError;
 
-            % Set sdot to -k*sign(s)
-            u(timeInd) = (-thetaDDotError - lambda*thetaDotError - k*sign(s))/params.sys.B(1);
+    % if s is large, recalulate control input
+    if abs(s(timeInd)) > phi || timeInd == 1
+        % Horizon indices
+        hInds = (timeInd:min((timeInd+HorizonSampling-1),length(t)));
 
-            % Select closest force
-            % [~,controlInd] = min(abs(params.controlInputSet-u(timeInd)));
-            % u(timeInd) = params.controlInputSet(controlInd);
+        xStar = [thetaOpt(hInds),thetaDotOpt(hInds),zeros(length(hInds),2)];
 
-            % Quantize u based on force options
-            if s > 0
-                % Pick the largest control option still smaller than u
-                smallerOptions = params.controlInputSet(find(params.controlInputSet<=u(timeInd)));
-                if ~isempty(smallerOptions)
-                    u(timeInd) = max(smallerOptions);
-                else
-                    u(timeInd) = min(params.controlInputSet);
-                end
-            else
-                % Pick the smallest option still larger than u
-                largerOptions = params.controlInputSet(find(params.controlInputSet>=u(timeInd)));
-                if ~isempty(largerOptions)
-                    u(timeInd) = min(largerOptions);
-                else
-                    u(timeInd) = max(params.controlInputSet);
-                end
-            end % Quantize u based on force options
+        lastSwitchInd = max([find(diff(u(1:max(timeInd-1,1)))~=0,1,'last'),1]);
+        timeSinceLastSwitch = t(timeInd)-t(lastSwitchInd);
+        previousInput = find(u(lastSwitchInd+1)==params.controlInputSet(:));
+        J = 1*exp(-10*timeSinceLastSwitch) * ones(size(params.controlInputSet(:)));
+        J(previousInput) = 0;
 
-        else % If s is small, use previous control input
-            u(timeInd) = u(timeInd-1);
-        end % if statement on size of s
+        % Simulate future times for each control input
+        for uInd = 1:length(params.controlInputSet(:))
 
-    else % Times between control steps use zero order hold
+            % Control inputs:
+            uOption = params.controlInputSet(uInd);
+
+            xHat = lsim(params.sys,uOption+Texc(hInds),t(hInds),states(:,timeInd));
+
+            S = (xHat - xStar)*[1; lambda; 0; 0];
+
+            % J(uInd) = J(uInd) + sum(S.^2);
+            J(uInd) = J(uInd) + abs(S(end));
+        end
+        [~,uOptInd] = min(J);
+   
+        u(timeInd) = params.controlInputSet(uOptInd);
+
+
+    else % If s is small, use previous control input
         u(timeInd) = u(timeInd-1);
-    end
-
+    end % if statement on size of s
 
     % PI control
-        % u(timeInd) = -1*(Kp*thetaDot(timeInd) + Ki*theta(timeInd));
+    % u(timeInd) = -1*(Kp*thetaDot(timeInd) + Ki*theta(timeInd));
 
     % Forward Euler Step
-    states(:,timeInd + 1) = states(:,timeInd) + (params.sys.A*states(:,timeInd) + params.sys.B*(u(timeInd)+Texc(timeInd)))*dt;
+    % states(:,timeInd + 1) = states(:,timeInd) + (params.sys.A*states(:,timeInd) + params.sys.B*(u(timeInd)+Texc(timeInd)))*dt;
+    
+    % Forward with lsim
+    stepInds = [timeInd,timeInd+1];
+    statesMany = lsim(params.sys,u(timeInd)+Texc(stepInds),t(stepInds),states(:,timeInd));
+    states(:,timeInd + 1) = statesMany(end,:);
 end
+close(waitbarObj)
 
 % For the last time step
 thetaDot(timeInd+1) = states(1,timeInd+1);
@@ -189,8 +290,18 @@ avePow = trapz(t(rampStartInd:end),mechPower(rampStartInd:end))/(params.finalTim
 switchInds = find(diff(u) ~= 0) + 1;  % +1 to get the index of the new value
 switchRate = length(switchInds)/finalTime
 
+%% Output
+out = struct();
+out.u = u;
+out.t = t;
+out.theta = theta;
+out.thetaDot = thetaDot;
+out.optimalAvergePower = avePowOpt;
+out.averagePower = avePow;
+out.mechanicalPower = mechPower;
+out.switchRate = switchRate;
 
-%% Displace results
+%% Display results
 
 disp([num2str((avePowOpt-avePow)/avePowOpt*100),'% from optimal'])
 
@@ -200,7 +311,7 @@ if params.plotFigures
     subplot(222), yyaxis left, plot(t,thetaDot), ylabel('Angular Velocity [rad/s]'), grid
                yyaxis right,plot(t,Texc), xlabel('Time [s]'), ylabel('Excitaiton Torque [Nm]')
     subplot(223), plot(t,thetaDot,'k',t,thetaDotOpt,'k--'), xlabel('Time [s]'), ylabel('Angular Velocity [W]'), legend('Actual','Optimal'), grid
-    subplot(224), plot(t,[0,S]), xlabel('Time [s]'), ylabel('Sliding Surface'), grid
+    subplot(224), plot(t,[0,s]), xlabel('Time [s]'), ylabel('Sliding Surface'), grid
 end
 
 end
