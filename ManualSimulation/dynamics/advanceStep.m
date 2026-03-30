f5aadb406732e2639910b0f7f7bb7269d8fea8d0
jwills801 <jwills@zagmail.gonzaga.edu>
2026-03-30 15:49:38 -0500

    Add end stops

--- ManualSimulation/dynamics/advanceStep.m
+++ ManualSimulation/dynamics/advanceStep.m
@@ -1,9 +1,18 @@
 function finalState = advanceStep(initialState,dt,sys,T)
 
+% If the position is near the end stops, apply an end stop force
+pos = initialState(2);
+vel = initialState(1);
+if abs(pos) > pi/4
+    endStopTorque = -pos*1e7 - vel*1e7;
+else
+    endStopTorque = 0;
+end
+
+% Calculate xdot
+xDot = sys.A*initialState + sys.B*(T+endStopTorque) ;
+
 % Forward euler one step
-    finalState = initialState + (sys.A*initialState + sys.B*T)*dt;
+    finalState = initialState + xDot*dt;
     
-% lsim (takes many steps)
-    % statesFine = lsim(sys,[T T],[0 dt],initialState);
-    % finalState = statesFine(end,:);
 end
